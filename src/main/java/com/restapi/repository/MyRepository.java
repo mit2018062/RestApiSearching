874f9a18ec1a808752ee986e02820b97eb5aed66
Chaubey95 <mit2018043@iiita.ac.in>
2020-09-30 05:09:15 +0000

    updated mongoimport

--- src/main/java/com/restapi/repository/MyRepository.java
+++ src/main/java/com/restapi/repository/MyRepository.java
@@ -6,13 +6,15 @@ import com.restapi.model.Patient;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MyRepository extends MongoRepository<Patient, String> {
+public interface MyRepository extends MongoRepository<Patient, String> , PagingAndSortingRepository <Patient, String> {
 
     //List<Patient> getByName(String name);
     
     @Query(value = "{$or:[{firstname:{$regex:?0,$options:'i'}},{lastname:{$regex:?0,$options:'i'}}]}")
     List<Patient> getByFnameAndLname(String name);
 
+        
     
 }
