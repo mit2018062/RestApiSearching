0c57ad9706a0059f6ea2feb4685a0509d7906d78
KumariAnamika52 <72122875+KumariAnamika52@users.noreply.github.com>
2020-10-06 18:03:36 +0530

    Update PatientIdRepository.java

--- src/main/java/com/restapi/repository/PatientIdRepository.java
+++ src/main/java/com/restapi/repository/PatientIdRepository.java
@@ -2,8 +2,9 @@ package com.restapi.repository;
 
 import com.restapi.model.PatientId;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-public interface PatientIdRepository extends MongoRepository<PatientId, String>{
+//import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
+public interface PatientIdRepository extends CrudRepository<PatientId, String>{
    // public storePid(Stirng pid)
     
 }
